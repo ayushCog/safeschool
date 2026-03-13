19e652ae8c43ce83cb450c58d0c9f2eacf71bdfa
Anshul gaur <anshul@git.training>
2026-03-13 10:00:30 +0530

    Added two more entities (ComplianceRecord and Report)

---
+++ src/main/java/com/cognizant/safeschool/entity/ComplianceRecord.java
@@ -0,0 +1,24 @@
+package com.cognizant.safeschool.entity;
+
+import java.time.LocalDate;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ComplianceRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long complianceId;
+
+    private Long entityId;
+
+    private String type;
+    private String result;
+    private LocalDate date;
+    private String notes;
+}
